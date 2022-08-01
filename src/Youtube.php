7ccf8ae434b889a211d08d560385a64f1639b0cf
Pascal Canadas <pascal@mirandais.ovh>
2022-08-01 19:30:14 +0200

    🐛 update php 8 and dependencies

--- src/Youtube.php
+++ src/Youtube.php
@@ -3,6 +3,7 @@
 namespace Dawson\Youtube;
 
 use Exception;
+use Carbon\Carbon;
 use Google_Client;
 use Google_Service_YouTube;
 use Illuminate\Support\Facades\DB;
@@ -29,6 +30,13 @@ class Youtube
      * @var \Google_Service_YouTube
      */
     protected $youtube;
+
+    /**
+     * Google YouTube Service
+     *
+     * @var \Google_Service_YouTubePartner
+     */
+    protected $youtubePartner;
 
     /**
      * Video ID
@@ -62,7 +70,11 @@ class Youtube
 
         $this->client = $this->setup($client);
 
+        // Define an object that will be used to make all API requests.
         $this->youtube = new \Google_Service_YouTube($this->client);
+
+        // YouTube Partner object used to make Content ID API requests.
+        $this->youtubePartner = new \Google_Service_YouTubePartner($this->client);
 
         if ($accessToken = $this->getLatestAccessTokenFromDB()) {
             $this->client->setAccessToken($accessToken);
@@ -73,28 +85,24 @@ class Youtube
      * Upload the video to YouTube
      *
      * @param  string $path
-     * @param  array $data
+     * @param  array  $data
      * @param  string $privacyStatus
-     * @return self
-     * @throws Exception
+     * @return string
      */
     public function upload($path, array $data = [], $privacyStatus = 'public')
     {
-        if(!file_exists($path)) {
-            throw new Exception('Video file does not exist at path: "'. $path .'". Provide a full path to the file before attempting to upload.');
+        if (!file_exists($path)) {
+            throw new Exception('Video file does not exist at path: "' . $path . '". Provide a full path to the file before attempting to upload.');
         }
 
         $this->handleAccessToken();
 
         try {
             $video = $this->getVideo($data, $privacyStatus);
-
             // Set the Chunk Size
             $chunkSize = 1 * 1024 * 1024;
-
             // Set the defer to true
             $this->client->setDefer(true);
-
             // Build the request
             $insert = $this->youtube->videos->insert('status,snippet', $video);
 
@@ -129,44 +137,7 @@ class Youtube
 
             // Set the Snippet from Uploaded Video
             $this->snippet = $status['snippet'];
-
-        }  catch (\Google_Service_Exception $e) {
-            throw new Exception($e->getMessage());
-        } catch (\Google_Exception $e) {
-            throw new Exception($e->getMessage());
-        }
-
-        return $this;
-    }
-
-    /**
-     * Update the video on YouTube
-     *
-     * @param  string $id
-     * @param  array $data
-     * @param  string $privacyStatus
-     * @return self
-     * @throws Exception
-     */
-    public function update($id, array $data = [], $privacyStatus = 'public')
-    {
-        $this->handleAccessToken();
-
-        if (!$this->exists($id)) {
-            throw new Exception('A video matching id "'. $id .'" could not be found.');
-        }
-
-        try {
-            $video = $this->getVideo($data, $privacyStatus, $id);
-
-            $status = $this->youtube->videos->update('status,snippet', $video);
-
-            // Set ID of the Updated Video
-            $this->videoId = $status['id'];
-
-            // Set the Snippet from Updated Video
-            $this->snippet = $status['snippet'];
-        }  catch (\Google_Service_Exception $e) {
+        } catch (\Google_Service_Exception $e) {
             throw new Exception($e->getMessage());
         } catch (\Google_Exception $e) {
             throw new Exception($e->getMessage());
@@ -178,9 +149,9 @@ class Youtube
     /**
      * Set a Custom Thumbnail for the Upload
      *
-     * @param  string $imagePath
+     * @param  string  $imagePath
+     *
      * @return self
-     * @throws Exception
      */
     public function withThumbnail($imagePath)
     {
@@ -215,7 +186,6 @@ class Youtube
 
             $this->client->setDefer(false);
             $this->thumbnailUrl = $status['items'][0]['default']['url'];
-
         } catch (\Google_Service_Exception $e) {
             throw new Exception($e->getMessage());
         } catch (\Google_Exception $e) {
@@ -226,18 +196,84 @@ class Youtube
     }
 
     /**
+     * Set the video status by id
+     *
+     * @param  int  $video_id
+     * @param  string  $status
+     *
+     * @return bool
+     */
+    public function setStatus($video_id, $status = 'unlisted')
+    {
+        $listResponse = $this->youtube->videos->listVideos("status", array('id' => $video_id));
+
+        // If $listResponse is empty, the specified video was not found.
+        if (!isset($listResponse[0])) {
+            return false; // No video exists
+        } else {
+            // Since the request specified a video ID, the response only contains one video resource.
+            $video = $listResponse[0];
+
+            $videoStatus = $video['status'];
+            $videoStatus->privacyStatus = $status;
+
+            // Set the status
+            $video->setStatus($videoStatus);
+
+            // Update the video resource by calling the videos.update() method.
+            $updateResponse = $this->youtube->videos->update("status", $video);
+        }
+
+        return $updateResponse;
+    }
+
+    /**
+     * Set the video snippet by if
+     *
+     * @param  int  $video_id
+     * @param  string  $title
+     * @param  string  $description
+     * @param  array  $tags
+     *
+     * @return 
+     */
+    public function setSnippet($video_id, $title = '', $description = '', $tags = array())
+    {
+        $listResponse = $this->youtube->videos->listVideos("snippet", array('id' => $video_id));
+
+        // If $listResponse is empty, the specified video was not found.
+        if (!isset($listResponse[0])) {
+            return sprintf('<h3>Can\'t find a video with video id: %s</h3>', $video_id);
+        } else {
+            // Since the request specified a video ID, the response only contains one video resource.
+            $video = $listResponse[0];
+
+            $videoSnippet = $video['snippet'];
+            $videoSnippet->title = $title;
+            $videoSnippet->description = $description;
+            $videoSnippet->tags = $tags;
+
+            // Set the status
+            $video->setSnippet($videoSnippet);
+
+            // Update the video resource by calling the videos.update() method.
+            $updateResponse = $this->youtube->videos->update("snippet", $video);
+        }
+    }
+
+    /**
      * Delete a YouTube video by it's ID.
      *
-     * @param  int $id
+     * @param  int  $id
+     *
      * @return bool
-     * @throws Exception
      */
     public function delete($id)
     {
         $this->handleAccessToken();
 
         if (!$this->exists($id)) {
-            throw new Exception('A video matching id "'. $id .'" could not be found.');
+            throw new Exception('A video matching id "' . $id . '" could not be found.');
         }
 
         return $this->youtube->videos->delete($id);
@@ -253,26 +289,20 @@ class Youtube
     {
         // Setup the Snippet
         $snippet = new \Google_Service_YouTube_VideoSnippet();
-
         if (array_key_exists('title', $data))       $snippet->setTitle($data['title']);
         if (array_key_exists('description', $data)) $snippet->setDescription($data['description']);
         if (array_key_exists('tags', $data))        $snippet->setTags($data['tags']);
         if (array_key_exists('category_id', $data)) $snippet->setCategoryId($data['category_id']);
-
         // Set the Privacy Status
         $status = new \Google_Service_YouTube_VideoStatus();
         $status->privacyStatus = $privacyStatus;
-
         // Set the Snippet & Status
         $video = new \Google_Service_YouTube_Video();
-        if ($id)
-        {
+        if ($id) {
             $video->setId($id);
         }
-
         $video->setSnippet($snippet);
         $video->setStatus($status);
-
         return $video;
     }
 
@@ -292,6 +322,51 @@ class Youtube
         if (empty($response->items)) return false;
 
         return true;
+    }
+
+    /**
+     * Return the Video duration
+     * @param  int  $video_id
+     * 
+     * @return int
+     */
+    public function getDuration($video_id)
+    {
+        $listResponse = $this->youtube->videos->listVideos("contentDetails", array('id' => $video_id));
+
+        // If $listResponse is empty, the specified video was not found.
+        if (!isset($listResponse[0])) {
+            return sprintf('<h3>Can\'t find a video with video id: %s</h3>', $video_id);
+        } else {
+            $seconds = 0;
+            if (isset($listResponse[0]['contentDetails']) && isset($listResponse[0]['contentDetails']['duration'])) {
+                // Since the request specified a video ID, the response only contains one video resource.
+                $duration = $listResponse[0]['contentDetails']['duration'];
+                $seconds = $this->ISO8601ToSeconds($duration);
+            }
+
+            return $seconds;
+        }
+    }
+
+    /**
+     * Converts Time ISO-8601 to secs
+     *
+     * @return int
+     */
+    public static function ISO8601ToSeconds($ISO8601)
+    {
+        preg_match('/(\d{1,2})[H]/', $ISO8601, $hours);
+        preg_match('/(\d{1,2})[M]/', $ISO8601, $minutes);
+        preg_match('/(\d{1,2})[S]/', $ISO8601, $seconds);
+
+        $hours = isset($hours[1]) ? $hours[1] : 0;
+        $minutes = isset($minutes[1]) ? $minutes[1] : 0;
+        $seconds = isset($seconds[1]) ? $seconds[1] : 0;
+
+        $totalSeconds = ($hours * 60 * 60) + ($minutes * 60) + $seconds;
+
+        return $totalSeconds;
     }
 
     /**
@@ -327,13 +402,12 @@ class Youtube
     /**
      * Setup the Google Client
      *
-     * @param Google_Client $client
-     * @return Google_Client $client
-     * @throws Exception
+     * @param \Google_Client $client
+     * @return \Google_Client $client
      */
     private function setup(Google_Client $client)
     {
-        if(
+        if (
             !$this->app->config->get('youtube.client_id') ||
             !$this->app->config->get('youtube.client_secret')
         ) {
@@ -347,8 +421,8 @@ class Youtube
         $client->setApprovalPrompt('force');
         $client->setRedirectUri(url(
             $this->app->config->get('youtube.routes.prefix')
-            . '/' .
-            $this->app->config->get('youtube.routes.redirect_uri')
+                . '/' .
+                $this->app->config->get('youtube.routes.redirect_uri')
         ));
 
         return $this->client = $client;
@@ -363,7 +437,7 @@ class Youtube
     {
         return DB::table('youtube_access_tokens')->insert([
             'access_token' => json_encode($accessToken),
-            'created_at'   => (new \DateTime())->setTimestamp($accessToken['created']),
+            'created_at'   => Carbon::createFromTimestamp($accessToken['created'])
         ]);
     }
 
@@ -375,10 +449,10 @@ class Youtube
     public function getLatestAccessTokenFromDB()
     {
         $latest = DB::table('youtube_access_tokens')
-                    ->latest('created_at')
-                    ->first();
+            ->latest('created_at')
+            ->first();
 
-        return $latest ? (is_array($latest) ? $latest['access_token'] : $latest->access_token ) : null;
+        return $latest ? (is_array($latest) ? $latest['access_token'] : $latest->access_token) : null;
     }
 
     /**
@@ -392,11 +466,9 @@ class Youtube
             throw new \Exception('An access token is required.');
         }
 
-        if($this->client->isAccessTokenExpired())
-        {
+        if ($this->client->isAccessTokenExpired()) {
             // If we have a "refresh_token"
-            if (array_key_exists('refresh_token', $accessToken))
-            {
+            if (array_key_exists('refresh_token', $accessToken)) {
                 // Refresh the access token
                 $this->client->refreshToken($accessToken['refresh_token']);
 
--- src/YoutubeServiceProvider.php
+++ src/YoutubeServiceProvider.php
@@ -18,18 +18,18 @@ class YoutubeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $config = realpath(__DIR__.'/../config/youtube.php');
+        $config = realpath(__DIR__ . '/../config/youtube.php');
 
         $this->publishes([$config => config_path('youtube.php')], 'config');
 
         $this->mergeConfigFrom($config, 'youtube');
 
         $this->publishes([
-            __DIR__.'/../migrations/' => database_path('migrations')
+            __DIR__ . '/../migrations/' => database_path('migrations')
         ], 'migrations');
 
-        if($this->app->config->get('youtube.routes.enabled')) {
-            include __DIR__.'/../routes/web.php';
+        if ($this->app->config->get('youtube.routes.enabled')) {
+            include __DIR__ . '/../routes/web.php';
         }
     }
 
@@ -38,7 +38,7 @@ class YoutubeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('youtube', function($app) {
+        $this->app->singleton('youtube', function ($app) {
             return new Youtube($app, new \Google_Client);
         });
     }
